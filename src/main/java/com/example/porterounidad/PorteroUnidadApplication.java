f15b98fe74ad2e130db175360a6c547c87a7d03a
juan9507 <juandavidnaranjo75@gmail.com>
2022-10-30 09:21:57 -0500

    creacio de proyecto para el reto rabbitMQ

---
+++ src/main/java/com/example/porterounidad/PorteroUnidadApplication.java
@@ -0,0 +1,13 @@
+package com.example.porterounidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PorteroUnidadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PorteroUnidadApplication.class, args);
+	}
+
+}
